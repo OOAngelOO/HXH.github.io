622c9aa19a2a81df8b1ea5b0ef17a5566ca8a781
OOAngelOO <30968451+OOAngelOO@users.noreply.github.com>
2018-12-27 15:12:10 +0800

    Add files via upload
    第一个上传到github上的代码

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,26 @@
+// 获取文档中标签是h1的元素
+var myImage=document.querySelector('img');
+myImage.onclick=function(){
+    var mySrc=myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png'){
+        myImage.setAttribute('src', 'images/11.jpg');
+    }else {
+        myImage.setAttribute('src', 'images/firefox-icon.png')
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName(){
+    var myName=prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent='Mozilla is cool,'+myName
+}
+if(!localStorage.getItem('name')){
+    setUserName()
+}else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent= 'Mozilla is cool,'+storedName
+}
+myButton.onclick=function(){
+    setUserName();
+}
